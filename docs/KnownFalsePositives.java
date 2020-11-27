2d8a5f8d4c638a55ac44912f2a73450b67a339d1
SOFe <sofe2038@gmail.com>
2020-11-27 18:56:10 +0800

    Fixed some typos

--- docs/KnownFalsePositives.java
+++ docs/KnownFalsePositives.java
@@ -2,16 +2,11 @@ class KnownFalsePositives {
 	/**
 	 * This is a false positive due to multiple return arms.
 	 *
-	 * The CFG contains an edge (secret -> Return).
+	 * The CFG contains an edge (secret -&gt; Return).
 	 * But in fact, Return is always 1.
 	 * This is considered a minor use case not to be fixed,
 	 * because duplicated code in multiple return arms
 	 * is typically regarded as an antipattern anyway.
-	 *
-	 * However, this pattern is checked as a special case
-	 * if multiple arms return the same literal,
-	 * including string literals, int literals and boolean literals,
-	 * but not for class constant literals.
 	 */
 	int foo(int x) {
 		boolean secret = sourceMarker(1);
